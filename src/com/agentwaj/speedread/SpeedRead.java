cea77887e3308a42f66e46e3786595e9ea8e6970
Wajahat Siddiqui <agentwaj@gmail.com>
2014-04-13 10:12:45 -0700

    Automatically refresh card scroll view.

--- src/com/agentwaj/speedread/SpeedRead.java
+++ src/com/agentwaj/speedread/SpeedRead.java
@@ -1,6 +1,8 @@
 package com.agentwaj.speedread;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +11,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.FileObserver;
 import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.glass.app.Card;
+import com.google.android.glass.media.CameraManager;
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
 import com.google.android.glass.touchpad.GestureDetector.BaseListener;
@@ -32,6 +36,8 @@ public class SpeedRead extends Activity {
 	
 	// Allows for horizontal scrolling between cards
 	private CardScrollView mCardScrollView;
+	
+	private CustomCardScrollAdapter mAdapter;
 	
 	// Handles gesture input
 	private GestureDetector mGestureDetector;
@@ -52,8 +58,8 @@ public class SpeedRead extends Activity {
 		
 		// Link the cards to the scroll view then display the scroll view
 		mCardScrollView = new CardScrollView(this);
-		CustomCardScrollAdapter adapter = new CustomCardScrollAdapter();
-		mCardScrollView.setAdapter(adapter);
+		mAdapter = new CustomCardScrollAdapter();
+		mCardScrollView.setAdapter(mAdapter);
 		mCardScrollView.activate();
 		// Capture is initial screen, settings to left and stories to right
 		mCardScrollView.setSelection(1);
@@ -80,6 +86,7 @@ public class SpeedRead extends Activity {
 		for (String story : savedStories) {
 			card = new Card(this);
 			card.setText(story);
+			card.setFootnote("Story");
 			mCards.add(card);
 		}
 	}
@@ -104,9 +111,10 @@ public class SpeedRead extends Activity {
 					startActivityForResult(i, 1);
 					break;
 				default: // Stories
-					Toast.makeText(SpeedRead.this, mCards.get(position).getText(), Toast.LENGTH_SHORT).show();
+					// Remove a card from the immersion
 					mPrefs.edit().remove(mCards.get(position).getText()).commit();
 					mCards.remove(mCards.get(position));
+					mAdapter.notifyDataSetChanged();
 				}
 				return true;
 			}
@@ -117,14 +125,68 @@ public class SpeedRead extends Activity {
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Process a picture once it is ready
 		if (requestCode == 1 && resultCode == RESULT_OK && mPrefs != null) {
-			mPrefs.edit().putString(System.currentTimeMillis() + "", "test").commit();
-			Toast.makeText(SpeedRead.this, "SUCCESS BUDDY!", Toast.LENGTH_SHORT).show();
-		} else {
-			Toast.makeText(SpeedRead.this, "Bummer!", Toast.LENGTH_SHORT).show();
+			String picturePath = data.getStringExtra(CameraManager.EXTRA_PICTURE_FILE_PATH);
+			processPictureWhenReady(picturePath);
+			mAdapter.notifyDataSetChanged();
 		}
 		
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	/*
+	 * Process a picture once it is ready
+	 */
+	private void processPictureWhenReady(final String picturePath) {
+		final File pictureFile = new File(picturePath);
+		
+		if (pictureFile.exists()) {
+			// The picture is ready; process it.
+			
+			// Save a card
+			Calendar c = Calendar.getInstance();
+			String time = c.get(Calendar.MONTH) + "/" + 
+					c.get(Calendar.DATE) + ", " + 
+					c.get(Calendar.HOUR_OF_DAY) + ":" + 
+					c.get(Calendar.MINUTE);
+			mPrefs.edit().putString(time, "Rest of text").commit();
+			mCards.add(new Card(this).setText(time).setFootnote("Story"));
+			mAdapter.notifyDataSetChanged();
+		} else {
+			// The file does not exist yet.
+			final File parentDirectory = pictureFile.getParentFile();
+			FileObserver observer = new FileObserver(parentDirectory.getPath()) {
+				
+				// Protect against additional pending events after CLOSE_WRITE is handled.
+				private boolean isFileWritten;
+				
+				@Override
+				public void onEvent(int event, String path) {
+					if (!isFileWritten) {
+						// For safety, make sure that the file was created in 
+						// the directory is actually the one we're expecting.
+						File affectedFile = new File(parentDirectory, path);
+						isFileWritten = (event == FileObserver.CLOSE_WRITE 
+								&& affectedFile.equals(pictureFile));
+						
+						if (isFileWritten) {
+							stopWatching();
+							
+							// Now that the file is ready, recursively call 
+							// processPictureWhenReady again (on the UI thread).
+							runOnUiThread(new Runnable() {
+								@Override
+								public void run() {
+									processPictureWhenReady(picturePath);
+								}
+							});
+						}
+					}
+				}
+			};
+			observer.startWatching();
+		}
 	}
 	
 	/*
